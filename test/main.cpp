ab38e703f78d7f3b44f378dd516f5cb5f20c8a82
artur <babalasoft@gmail.com>
2022-02-17 13:46:06 +0100

    Add LinuxApplicationFabric unit test

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,22 @@
 #include <gtest/gtest.h>
+#include "../src/ApplicationFabric/LinuxApplicationFabric.h"
+
 
 int main(int argc, char** argv) {
   // Demonstrate some basic assertions.
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
 TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
+}
+TEST(LinuxApplicationFabric, BasicAssertions) {
+    LinuxApplicationFabric linuxAppFabric;
+    IApplication* app = linuxAppFabric.createApplication();
+    // Expect two strings not to be equal.
+    EXPECT_NE(app, nullptr);
 }
